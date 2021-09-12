6bf14744da7f41cfeceb58c40a9d0c2ec25cd08f
Ken Nguyen <nguyenngocvy2005@gmail.com>
2021-09-12 19:41:51 +0700

    them tinh nang 1

---
+++ myfirst.js
@@ -0,0 +1,12 @@
+var http=require("http");
+//var dt=require("./myfirstmodule");
+var fs=require('fs');
+
+http.createServer(function(req, res){
+    fs.readFile('demofile1.html',function(err, data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        //res.write("Hom nay la ngay: "+dt.myDate()+ "Mua: " + req.url);
+        res.write(data);
+        return res.end;
+    });    
+}).listen(7777);
